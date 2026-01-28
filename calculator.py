b9d75e48a84fd40d334894dbdfb5eaaca883bf8e
Braulio Almonte <brauliox2112@hotmail.com>
2026-01-28 13:08:31 -0400

    agregado multiplicacion division

--- calculator.py
+++ calculator.py
@@ -4,19 +4,21 @@ def suma(a,b):
 def resta(a,b):
     return a-b
 
-#Multipication va aqui
+def multi(a,b):
+    return a*b
 
-#Division va aqui
+def divi(a,b):
+    return a/b
+
 
 while True:
     print("Menu calculadora")
 
     print("1. Suma")
     print("2. Resta")
-    #Multiplicacion va aqui
-
-    #Division va aqui
-    print("3. Salir")
+    print("3. Multi")
+    print("4. Division")
+    print("5. Salir")
 
     usuario = input("")
 
@@ -33,9 +35,26 @@ while True:
             n1 = int(input("Inserte el primer numero: "))
             n2 = int(input("Inserte el segundo numero: "))
 
-            print(f"La resta de {n1} + {n2} es = {suma(n1,n2)}")
+            print(f"La resta de {n1} - {n2} es = {suma(n1,n2)}")
 
         case "3":
+            print("Multi")
+            n1 = int(input("Inserte el primer numero: "))
+            n2 = int(input("Inserte el segundo numero: "))
+
+            print(f"La resta de {n1} * {n2} es = {multi(n1,n2)}")
+
+        case "4":
+            print("Division")
+            n1 = int(input("Inserte el primer numero: "))
+            n2 = int(input("Inserte el segundo numero: "))
+
+            if n2 == 0:
+                print("No se puede dividir entre 0")
+            else:
+                print(f"La resta de {n1} * {n2} es = {multi(n1,n2)}")
+
+        case "5":
             print("Adios")
             break
 
